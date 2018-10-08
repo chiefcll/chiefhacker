29f0b0678d163994917e8c1c7f463d2b2fe3006c
Chris Lorenzo <Christopher_Lorenzo@Comcast.com>
2018-10-07 20:12:16 -0400

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+"use strict";
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('chiefhacker-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/speaking.html',
+        '/static/css/main.css',
+        '/static/img/profile.jpg',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+
+  let url = event.request.url;
+  if (/\.jpg$|.png$|.gif$|.webp$/.test(url)) {
+    // Connection Type is not available yet
+    // https://developer.mozilla.org/en-US/docs/Web/API/Network_Information_API
+    // navigator.connection.type === 'cellular'
+
+    let size = {
+      "2g": 'old',
+      "3g": 'head',
+      "4g": 'medium',
+      "slow-2g": 'xsmall'
+    }[navigator.connection.effectiveType];
+
+
+    event.respondWith(
+        fetch(url.replace(/\.[a-z]*$/, `_${size}$&`), {
+            mode: 'no-cors'
+        }).catch(function() {
+          return caches.match('/static/img/profile.jpg');
+        })
+    );
+  }
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
