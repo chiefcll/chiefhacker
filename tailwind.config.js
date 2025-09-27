ec9c020ef541f2f6ba2437633dc7b8c2dafbff9e
Chris Lorenzo <chiefcll@gmail.com>
2025-09-26 22:06:44 -0400

    chore: add PostCSS/Tailwind toolchain for careercanvas theme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./themes/careercanvas/layouts/**/*.{html,js}",
+    "./layouts/**/*.{html,js}",
+    "./content/**/*.{md,html}",
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
